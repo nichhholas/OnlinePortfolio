de8b672599062ffd9c69a225be5c1333e448c876
Nicholas Tan <nic@Nicholass-MacBook-Pro.local>
2019-11-27 19:33:12 +0800

    Version 1 portfolio

---
+++ public/contact.js
@@ -0,0 +1,9 @@
+function openLinkedIn(){
+	var win = window.open('https://www.linkedin.com/in/nicholas-tan-305575181/', '_blank');
+	win.focus();
+}
+
+function sendEmail(){
+	window.open('mailto:e0202624@u.nus.edu')
+}
+
